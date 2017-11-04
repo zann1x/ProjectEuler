21344fdf43429f49337b05a377701bca38c8fac6
zann1x <zannix@freenet.de>
2017-11-04 19:34:46 +0100

    Solved problem 30

--- src/Problem030.java
+++ src/Problem030.java
@@ -1,22 +1,49 @@
 /*
  * Find the sum of all the numbers that can be written as the sum of fifth powers of their digits.
- * Answer:
+ * Answer: 443839
  */
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Problem030 {
 
-    public void solve() {
-        int fifthPowers[] = new int[10];
-        for (int i = 0; i < 10; i++) {
+    private void buildFifthPowers(int[] fifthPowers) {
+        for (int i = 0; i < fifthPowers.length; i++) {
             fifthPowers[i] = i;
             for (int j = 0; j < 4; j++) {
                 fifthPowers[i] *= i;
             }
         }
+    }
 
-        for (int i = 1; i < 355000; i++) { // upper bound -> 6 * 9^5
-            // TODO
+    public void solve() {
+        int fifthPowers[] = new int[10];
+        List<Integer> numbers = new ArrayList<>();
+        
+        buildFifthPowers(fifthPowers);
+
+        int sum;
+        for (int i = 2; i < 355000; i++) { // upper bound -> 6 * 9^5
+            sum = 0;
+            int tmp = i;
+            while (tmp > 0) {
+                int num = tmp % 10;
+                tmp /= 10;
+
+                sum += fifthPowers[num];
+            }
+
+            if (sum == i) {
+                numbers.add(sum);
+            }
         }
+
+        sum = 0;
+        for (int number : numbers) {
+            sum += number;
+        }
+        System.out.println(sum);
     }
 
     public static void main(String[] args) {
